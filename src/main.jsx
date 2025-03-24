eec9d8baa8f9ae35c6b715bc575d5e3b10a78e33
The Zuraiz <thezuraiz@gmail.com>
2025-03-24 16:04:08 +0500

    Return to previous Commit

--- src/main.jsx
+++ src/main.jsx
@@ -10,9 +10,9 @@ import { createRoot } from "react-dom/client";
 // import CustomHook from "./component/CustomHook";
 // import App from "./App.jsx";
 // // import CounterAppUsingRedux from "./component/CounterAppUsingRedux.jsx";
-// import { Provider } from "react-redux";
+import { Provider } from "react-redux";
 
-// import userStore from "./store/userRedux.jsx";
+import userStore from "./store/userRedux.jsx";
 // import store from "./context/reduxStore.jsx";
 // import UseCounterComponent from "./component/UseCounterComponent.jsx";
 // import UseRefHook from "./hooks/useRefHook";
@@ -22,53 +22,52 @@ import { createRoot } from "react-dom/client";
 // import UseReducerHook from "./hooks/useReducer";
 // import UseMemoHook from "./hooks/useMemoHook";
 // import UseCallBackHook from "./hooks/useCallBackHook";
-// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-// import { Slide, ToastContainer } from "react-toastify";
-// import TodoApp from "./apps/todo_app.jsx";
-import CounterApp from "./apps/hoc/CounterApp.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Slide, ToastContainer } from "react-toastify";
+import TodoApp from "./apps/todo_app.jsx";
+import { BrowserRouter } from "react-router-dom";
 
-// const queryClient = new QueryClient();
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
-  <CounterApp />
-  // <BrowserRouter>
-  //   {/* <Provider store={store}>
-  //     <CounterAppUsingRedux />
-  //   </Provider> */}
+  <BrowserRouter>
+    {/* <Provider store={store}>
+      <CounterAppUsingRedux />
+    </Provider> */}
 
-  //   {/* <UseCounterComponent /> */}
-  //   {/* <UseFetchHook /> */}
-  //   {/* <FormHandling /> */}
-  //   {/* <UseStateHook /> */}
-  //   {/* <CustomHook /> */}
-  //   {/* <UseLayoutEffect /> */}
-  //   {/* <UseImperativeHandleHook /> */}
-  //   {/* <UseRefHook /> */}
-  //   {/* <UseCallBackHook /> */}
-  //   {/* <UseMemoHook /> */}
-  //   {/* <UseReducerHook /> */}
-  //   {/* <UseContextHook /> */}
-  //   {/* <UseEffectComponent /> */}
-  //   <Provider store={userStore}>
-  //     <QueryClientProvider client={queryClient}>
-  //       {/* <App /> */}
-  //       <TodoApp />
-  //       <ToastContainer
-  //         position="bottom-left"
-  //         autoClose={2000}
-  //         hideProgressBar={false}
-  //         newestOnTop
-  //         closeOnClick={false}
-  //         rtl={false}
-  //         pauseOnFocusLoss
-  //         draggable
-  //         pauseOnHover
-  //         theme="light"
-  //         transition={Slide}
-  //       />
-  //       <ReactQueryDevtools initialIsOpen={false} />
-  //     </QueryClientProvider>
-  //   </Provider>
-  // </BrowserRouter>
+    {/* <UseCounterComponent /> */}
+    {/* <UseFetchHook /> */}
+    {/* <FormHandling /> */}
+    {/* <UseStateHook /> */}
+    {/* <CustomHook /> */}
+    {/* <UseLayoutEffect /> */}
+    {/* <UseImperativeHandleHook /> */}
+    {/* <UseRefHook /> */}
+    {/* <UseCallBackHook /> */}
+    {/* <UseMemoHook /> */}
+    {/* <UseReducerHook /> */}
+    {/* <UseContextHook /> */}
+    {/* <UseEffectComponent /> */}
+    <Provider store={userStore}>
+      <QueryClientProvider client={queryClient}>
+        {/* <App /> */}
+        <TodoApp />
+        <ToastContainer
+          position="bottom-left"
+          autoClose={2000}
+          hideProgressBar={false}
+          newestOnTop
+          closeOnClick={false}
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+          transition={Slide}
+        />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </Provider>
+  </BrowserRouter>
 );
